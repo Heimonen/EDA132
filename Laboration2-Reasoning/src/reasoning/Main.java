06d6a75b75fcfe04f77a4891d04f14672bee00e8
Christofer Heimonen <christofer.heimonen@lunicore.se>
2013-02-28 12:32:38 +0100

    Queries are now working.

--- Laboration2-Reasoning/src/reasoning/Main.java
+++ Laboration2-Reasoning/src/reasoning/Main.java
@@ -3,9 +3,8 @@ package reasoning;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
-
+import org.openrdf.model.Value;
 import org.openrdf.query.BindingSet;
-import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
 import org.openrdf.repository.Repository;
@@ -15,87 +14,76 @@ import org.openrdf.repository.http.HTTPRepository;
 
 public class Main {
 
-	private static String aQuery = "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>" +
+	private static Repository myRepository;
+	private static final String SESAME_SERVER = "http://asimov.ludat.lth.se/openrdf-sesame/";
+	private static final String REPOSITORY_ID = "KnowRob";
+	
+	private static String drinkingGlassStored = 
+		"PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
 		"PREFIX computable:<http://ias.cs.tum.edu/kb/computable.owl#> " +
-		"PREFIX owl2xml:<http://www.w3.org/2006/12/owl2-xml#>" +
-		"PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>" +
-		"PREFIX owl:<http://www.w3.org/2002/07/owl#>" +
-		"PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-		"PREFIX knowrob:<http://ias.cs.tum.edu/kb/knowrob.owl#>" +
-		"SELECT ?out" +
-		"WHERE" +
-		"{" +
-		    "knowrob:DrinkingGlass rdfs:subClassOf ?e ." +
-		    "?e rdfs:subClassOf ?d ." +
-		    "?d rdfs:subClassOf ?f ." +
-		    "?stored rdfs:domain ?f ." +
-		    "?node owl:onProperty ?stored ," +
-		    "?node owl:someValuesFrom ?out" +
+		"PREFIX owl2xml:<http://www.w3.org/2006/12/owl2-xml#> " +
+		"PREFIX xsd:<http://www.w3.org/2001/XMLSchema#> " +
+		"PREFIX owl:<http://www.w3.org/2002/07/owl#> " +
+		"PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+		"PREFIX knowrob:<http://ias.cs.tum.edu/kb/knowrob.owl#> " +
+		"SELECT ?out " +
+		"WHERE " +
+		"{ " +
+		    "knowrob:DrinkingGlass rdfs:subClassOf ?e . " +
+		    "?e rdfs:subClassOf ?d . " +
+		    "?d rdfs:subClassOf ?f . " +
+		    "?stored rdfs:domain ?f . " +
+		    "?node owl:onProperty ?stored . " +
+		    "?node owl:someValuesFrom ?out " +
 		"}";
 	
-	
-	
 	public static void main(String[] args) {
-		String sesameServer = "http://asimov.ludat.lth.se";
-		String repositoryID = "KnowRob";
-		Repository myRepository = new HTTPRepository(sesameServer, repositoryID);
+		myRepository = new HTTPRepository(SESAME_SERVER, REPOSITORY_ID);
 		try {
 			myRepository.initialize();
-			System.out.println("Done!");
 		} catch (RepositoryException e) {
 			e.printStackTrace();
 		}
-		try {
-			RepositoryConnection con = myRepository.getConnection();
-			try {
-				TupleQuery query = 
-					con.prepareTupleQuery(
-							org.openrdf.query.QueryLanguage.SPARQL, aQuery);
-				
-				TupleQueryResult qres = query.evaluate();
-                ArrayList<HashMap> reslist = new ArrayList<HashMap>();
+		System.out.println("KnowRob repository:" + "\n");
+		System.out.println("1. Where may a drinking glass be stored? (Explore the concept knowrob:DrinkingGlass by hand to find out the answer and to form your SPARQL query.)");
+		ArrayList<HashMap<String, Value>> list = runSPARQL(drinkingGlassStored);
+		HashMap<String, Value> value = list.get(0);
+		System.out.println("Answer: " + value.get("out") + "\n");
+		System.out.println("2. What are the \"putting\" actions a robot using KnowRob may reason about and what are their subactions? (Explore knowrob:Movement-TranslationEvent and knowrob:subAction.)");
+		System.out.println("Answer: ");
+	}
+	
+	/**
+     *  Execute a SELECT SPARQL query against the graph 
+     * 
+     * @param queryString SELECT SPARQL query
+     * @return list of solutions, each containing a HashMap<String, Value> of bindings
+     */
+    public static ArrayList<HashMap<String, Value>> runSPARQL(String queryString) {
+        try{
+            RepositoryConnection con = myRepository.getConnection();
+            try {
+                TupleQuery query = 
+                    con.prepareTupleQuery(
+                    org.openrdf.query.QueryLanguage.SPARQL, queryString);
+                TupleQueryResult qres = query.evaluate();
+                ArrayList<HashMap<String, Value>> reslist = new ArrayList<HashMap<String, Value>>();
                 while (qres.hasNext()) {
                     BindingSet b = qres.next();
-                    Set names = b.getBindingNames();
-                    HashMap hm = new HashMap();
+                    Set<String> names = b.getBindingNames();
+                    HashMap<String, Value> hm = new HashMap<String, Value>();
                     for (Object n : names) {
                         hm.put((String) n, b.getValue((String) n));
                     }
                     reslist.add(hm);
-                }    
-			} finally {
-				con.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-
-		System.out.println("Done with query!");
-
-
-		/*
-		PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
-		PREFIX computable:<http://ias.cs.tum.edu/kb/computable.owl#>
-		PREFIX owl2xml:<http://www.w3.org/2006/12/owl2-xml#>
-		PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
-		PREFIX owl:<http://www.w3.org/2002/07/owl#>
-		PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-		PREFIX knowrob:<http://ias.cs.tum.edu/kb/knowrob.owl#>
-
-		SELECT ?out
-		WHERE
-		{
-		    knowrob:DrinkingGlass rdfs:subClassOf ?e .
-		    ?e rdfs:subClassOf ?d .
-		    ?d rdfs:subClassOf ?f .
-		    ?stored rdfs:domain ?f .
-		    ?node owl:onProperty ?stored .
-		    ?node owl:someValuesFrom ?out
-		}
-		 */
-
-		//Documentation
-		//http://www.openrdf.org/doc/sesame2/users/ch08.html#d0e810
-	}
-
+                }
+                return reslist;
+            } finally {
+                con.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
