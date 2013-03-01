14b2f2754dd5b376ffb6d2d622d4d46a8db55358
Robert Borg <rauban@gmail.com>
2013-03-01 14:48:40 +0100

    started implementing model in c++

---
+++ Laboration3-DecisionTrees/src/example.h
@@ -0,0 +1,14 @@
+#include <utility>
+using std::pair;
+#include <vector>
+using std::vector;
+#include <string>
+
+class Example {
+	typedef pair<string, bool> Attribute;
+	typedef vector<Attribute> Attributes;
+public:
+
+private:
+
+};
