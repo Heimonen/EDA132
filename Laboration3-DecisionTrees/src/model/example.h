5826c25f82b28e41a205bbad35f039216347c75f
Robert Borg <rauban@gmail.com>
2013-03-01 16:51:08 +0100

    updated example class

--- Laboration3-DecisionTrees/src/model/example.h
+++ Laboration3-DecisionTrees/src/model/example.h
@@ -1,12 +1,8 @@
-#include <utility>
-using std::pair;
 #include <vector>
 using std::vector;
-#include <string>
 
 class Example {
-	typedef pair<string, bool> Attribute;
-	typedef vector<Attribute> Attributes;
+	typedef vector<unsigned int> Attributes;
 public:
 
 private:
